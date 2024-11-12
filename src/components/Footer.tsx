9682a26b385e5aa3af3f85448a3d7bc249a384b9
JhonatanGuarin <jhonatancamilo99@gmail.com>
2024-11-12 03:03:26 -0500

    Frontend

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Facebook, Twitter, Instagram } from 'lucide-react'
 
 export default function Footer() {
@@ -28,17 +29,7 @@ export default function Footer() {
               <br />
               Tel: 3219481516
             </p>
-            <div className="flex space-x-4">
-              <a href="#" className="text-gray-400 hover:text-primary dark:hover:text-primary-foreground transition-colors">
-                <Facebook size={20} />
-              </a>
-              <a href="#" className="text-gray-400 hover:text-primary dark:hover:text-primary-foreground transition-colors">
-                <Twitter size={20} />
-              </a>
-              <a href="#" className="text-gray-400 hover:text-primary dark:hover:text-primary-foreground transition-colors">
-                <Instagram size={20} />
-              </a>
-            </div>
+
           </div>
         </div>
         <div className="mt-8 pt-8 border-t border-gray-200 dark:border-gray-800 text-center text-sm text-muted-foreground dark:text-gray-400">
